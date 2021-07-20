a71d6f65e10c6b12467dcfb6343f85cf9273c521
premjitheisnam <hpremjit7@gmail.com>
2021-07-20 20:08:20 +0530

    imported newly created user.modules for using component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import {UsersModule} from './users/users.module'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
